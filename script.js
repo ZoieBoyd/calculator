0e7d52f64dc51fb52099f525467d18226ec48b1a
ZoieBoyd <zoieboyd@outlook.com>
2024-09-26 23:22:18 +0100

    Add functionality to calculator
    Implemented functionality to the majority of buttons on the GUI:
    - Added JavaScript to allow the text content of the calculator screen to dynamically update when number buttons are pressed.
    - Added logic to store the first operand and selected operator when an operator button is pressed.
    - Implemented JavaScript to change screen's text content to the calculation result when the equals button is selected.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let operator;
+const numBtns = document.querySelectorAll(".num-button");
+const opBtns = document.querySelectorAll(".operator-button");
+const equalBtn = document.querySelector(".equals-button");
+const clearBtn = document.querySelector(".clear-button");
+const screenText = document.querySelector(".screen");
+let displayValue = document.querySelector(".screen").textContent;
 let num1;
 let num2;
+let operator;
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -13,9 +19,56 @@ function operate(op, a, b) {
             return add(a, b);
         case "-":
             return subtract(a, b);
-        case "*":
+        case "×":
             return multiply(a, b);
-        case "/":
+        case "÷":
             return divide(a, b);
     }
 }
+
+function selectNumber() {
+    numBtns.forEach(numBtn => {
+        numBtn.addEventListener("click", (event) => {
+            if(screenText.textContent !== "0") {
+                screenText.textContent += numBtn.textContent; 
+            } else {
+                screenText.textContent = numBtn.textContent;
+            }
+            displayValue = document.querySelector(".screen").textContent;
+        });
+    });
+}
+
+function selectOperator() {
+    opBtns.forEach(opBtn => {
+        opBtn.addEventListener("click", (event) => {
+            operator = event.target.textContent;
+            if(num1 === undefined)
+            {
+                num1 = parseFloat(displayValue);
+            }
+            screenText.textContent = 0;
+        }
+    )});
+}
+
+function selectEquals() {
+    equalBtn.addEventListener("click", (event) => {
+        num2 = parseFloat(displayValue);
+        screenText.textContent = operate(operator, num1, num2);
+    });
+}
+
+function clear() {
+    clearBtn.addEventListener("click", (event) => {
+        num1 = undefined;
+        num2 = undefined;
+        operator = undefined;
+        screenText.textContent = 0;
+    });
+}
+
+selectNumber();
+selectOperator();
+selectEquals();
+clear();
