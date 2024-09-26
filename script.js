b9277e4e7ee6e731913c369b2d8fa47dfac9d8a5
ZoieBoyd <zoieboyd@outlook.com>
2024-09-26 20:00:00 +0100

    Implement calculator visual elememts
    - Changed HTML to include buttons of a classic calculator layout (numbers, clear, operators etc.).
    - Added CSS styling to the page's content to improve the visual appeal and replicate the iOS calculator GUI.
    - Implemeted basic JavaScript functions for each operator (add, subtract, multiply, divide).

---
+++ script.js
@@ -0,0 +1,21 @@
+let operator;
+let num1;
+let num2;
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(op, a, b) {
+    switch(op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
