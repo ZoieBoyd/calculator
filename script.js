39a10eafc4f7baec03a86cb5b0fa350528c1ead2
ZoieBoyd <zoieboyd@outlook.com>
2024-09-28 18:16:55 +0100

    Add visual effects for button presses
    - Implemented JavaScript to flip the colors of the operator button's text content and background when selected. This change will enhance user feedback clearly indicating which operator is currently active.
    - Added visual feedback to number button presses: the button changes to a lighter grey for 200 milliseconds before reverting to the original button color.
    - When an operator is selected, the screen text content no longer changes to 0, but will update only when the first digit of the 2nd number is input. This is to replicate iOS calculator behavior.

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let displayValue = document.querySelector(".screen").textContent;
 let num1;
 let num2;
 let operator;
+let operatorBtn;
+let isWaitingForNum2 = false;
+const orange = "#FF9500";
+const grey = "#333333cb";
+const lightGrey = "rgba(201, 201, 201, 0.603)";
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -29,12 +34,21 @@ function operate(op, a, b) {
 function selectNumber() {
     numBtns.forEach(numBtn => {
         numBtn.addEventListener("click", (event) => {
-            if(screenText.textContent !== "0") {
-                screenText.textContent += numBtn.textContent; 
+            const numValue = numBtn.textContent;
+            numBtn.style.backgroundColor = lightGrey;
+            setTimeout(() => {
+                numBtn.style.backgroundColor = grey; 
+            }, 200);
+            if (isWaitingForNum2) {
+                screenText.textContent = numValue;
+                isWaitingForNum2 = false;
+                operatorBtn.style.backgroundColor = orange;
+                operatorBtn.style.color = "white";
+
             } else {
-                screenText.textContent = numBtn.textContent;
+                screenText.textContent = screenText.textContent === "0" ? numValue : screenText.textContent + numValue;
             }
-            displayValue = document.querySelector(".screen").textContent;
+            displayValue = screenText.textContent;
         });
     });
 }
@@ -42,12 +56,16 @@ function selectNumber() {
 function selectOperator() {
     opBtns.forEach(opBtn => {
         opBtn.addEventListener("click", (event) => {
+            opBtns.forEach(opBtn => {
+                opBtn.style.backgroundColor = orange;
+                opBtn.style.color = "white";
+            });
             operator = event.target.textContent;
-            if(num1 === undefined)
-            {
-                num1 = parseFloat(displayValue);
-            }
-            screenText.textContent = 0;
+            operatorBtn = event.target;
+            event.target.style.backgroundColor = "white";
+            event.target.style.color = orange;
+            isWaitingForNum2 = true; 
+            num1 = parseFloat(displayValue);
         }
     )});
 }
