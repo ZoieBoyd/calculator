ec2895bff808290678d47830b4875e752974a5fb
ZoieBoyd <zoieboyd@outlook.com>
2024-10-11 17:26:07 +0100

    Refactor JavaScript for cleaner implementation
    - Refactored code to use event deleation, handling all button presses with a single event listener.
    - Renamed functions to be more suitable for their purpose.

--- script.js
+++ script.js
@@ -1,138 +1,126 @@
-const numBtns = document.querySelectorAll(".num-button");
-const opBtns = document.querySelectorAll(".operator-button");
-const funcBtns = document.querySelectorAll(".function-button");
-const decimalBtn = document.querySelector(".decimal-button");
-const equalBtn = document.querySelector(".equals-button");
-const clearBtn = document.querySelector(".clear-button");
-const plusMinusBtn = document.querySelector(".plus-minus-button")
-const percentBtn = document.querySelector(".percent-button");
+const calcBtns = document.querySelectorAll("button"); 
+const opBtns = document.querySelectorAll(".operator");
 const screenText = document.querySelector(".screen");
-let displayValue = document.querySelector(".screen").textContent;
 
 let num1;
 let num2;
 let operator;
-let operatorBtn;
+let currOpBtn;
 
 let isWaitingForNum2 = false;
 
 const orange = "#FF9500";
 const grey = "#333333cb";
 const lightGrey = "rgba(201, 201, 201, 0.603)";
-const midGrey= "rgb(170, 170, 170)";
+const midGrey = "rgb(170, 170, 170)";
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+calcBtns.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        switch(btn.className) {
+            case "number":
+                inputNumber(event.target);
+                break;
+            case "decimal":
+                inputDecimal(event.target);
+                break;
+            case "operator":
+                selectOperator(event.target);
+                break;
+            case "function clear":
+                clearScreen();
+                break;
+            case "function plus-minus":
+                invertSign();
+                break;
+            case "function percent":
+                convertPercent();
+                break;
+            case "equals":
+                selectEquals(event.target);
+                break;
+        }
+    });
+});
+
+function inputNumber(numBtn) {
+    buttonColorFlash(numBtn, grey, lightGrey);
+    const num = numBtn.textContent;
+    if (isWaitingForNum2) {
+        screenText.textContent = num;
+        currOpBtn.style.backgroundColor = orange;
+        currOpBtn.style.color = "white";
+        isWaitingForNum2 = false;
+    } else {
+        screenText.textContent = screenText.textContent === "0" ? num : screenText.textContent + num;
+    }
+}
+
+function inputDecimal(decimalBtn) {
+    buttonColorFlash(decimalBtn, grey, lightGrey);
+    if(!screenText.textContent.includes(".")) {
+        screenText.textContent += ".";
+    }
+}
+
+function selectOperator(opBtn) {
+    currOpBtn = opBtn;
+    operator = opBtn.textContent;
+    opBtns.forEach (currBtn => {
+        currBtn.style.backgroundColor = orange;
+        currBtn.style.color = "white";
+    });
+    opBtn.style.backgroundColor = "white";
+    opBtn.style.color = orange;
+    isWaitingForNum2 = true;
+    if (num1 == undefined || num1 && num2) { 
+        // Allows for chained calculations.
+        num1 = parseFloat(screenText.textContent);
+    } else {
+        num2 = parseFloat(screenText.textContent);
+    }
+}
+
+function clearScreen() {
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    screenText.textContent = 0; 
+    currOpBtn.style.backgroundColor = orange;
+    currOpBtn.style.color = "white";
+}
+
+function convertPercent() { 
+    screenText.textContent = parseFloat(screenText.textContent) / 100;
+}
+
+function invertSign() {
+    screenText.textContent = parseFloat(screenText.textContent) * -1;
+}
+
+function selectEquals(equalBtn) {
+    buttonColorFlash(equalBtn, orange, "white", "white", orange);
+    num2 = parseFloat(screenText.textContent);
+    const result = Math.round(parseFloat(operate(operator, num1, num2)) * 100000) / 100000; // Rounds the results to 5 decimal places
+    screenText.textContent = result;
+    num1 = parseFloat(screenText.textContent); // Allows for chained calculations.
+}
 
 function operate(op, a, b) {
     switch(op) {
         case "+":
-            return add(a, b);
+            return a + b;
         case "-":
-            return subtract(a, b);
+            return a - b;
         case "×":
-            return multiply(a, b);
+            return a * b;
         case "÷":
             if (b !== 0) {
-                return divide(a, b);
+                return a / b;
             } else {
                 window.alert("Error: Division by 0 is not allowed.");
                 return 0;
             }
     }
-}
-
-function selectNumber() {
-    numBtns.forEach(numBtn => {
-        numBtn.addEventListener("click", () => {
-            const numValue = numBtn.textContent;
-            buttonColorFlash(numBtn, grey, lightGrey);
-            if (isWaitingForNum2) {
-                screenText.textContent = numValue;
-                isWaitingForNum2 = false;
-                operatorBtn.style.backgroundColor = orange;
-                operatorBtn.style.color = "white";
-
-            } else {
-                screenText.textContent = screenText.textContent === "0" ? numValue : screenText.textContent + numValue;
-            }
-            displayValue = screenText.textContent;
-        });
-    });
-}
-
-function selectDecimal() {
-    decimalBtn.addEventListener("click", () => { 
-        if (!displayValue.includes(".")){
-            screenText.textContent += ".";
-            displayValue = screenText.textContent;
-        }
-    })
-}
-
-function selectOperator() {
-    opBtns.forEach(opBtn => {
-        opBtn.addEventListener("click", (event) => {
-            opBtns.forEach(opBtn => {
-                opBtn.style.backgroundColor = orange;
-                opBtn.style.color = "white";
-            });
-            operator = event.target.textContent;
-            operatorBtn = event.target;
-            event.target.style.backgroundColor = "white";
-            event.target.style.color = orange;
-            isWaitingForNum2 = true; 
-            if (num1 === undefined || num1 && num2) {
-                num1 = parseFloat(displayValue);
-            } else {
-                num2 = parseFloat(displayValue);
-            }
-        }
-    )});
-}
-
-function selectEquals() {
-    equalBtn.addEventListener("click", () => {
-        buttonColorFlash(equalBtn, orange, "white", "white", orange);
-        num2 = parseFloat(displayValue);
-        const result = Math.round(parseFloat(operate(operator, num1, num2)) * 100000) / 100000; // Rounds the results to 5 decimal places
-        screenText.textContent = result;
-        displayValue = screenText.textContent;
-        num1 = parseFloat(displayValue);
-    });
-}
-
-function clear() {
-    clearBtn.addEventListener("click", () => {
-        num1 = undefined;
-        num2 = undefined;
-        operator = undefined;
-        screenText.textContent = 0;
-    });
-}
-
-function selectFuncBtn(){
-    funcBtns.forEach(funcBtn => {
-        funcBtn.addEventListener("click", () => {
-            buttonColorFlash(funcBtn, midGrey, "white");
-        });
-    });
-}
-
-function selectPlusMinusBtn() {
-    plusMinusBtn.addEventListener("click", () => {
-        screenText.textContent =  parseFloat(screenText.textContent) * -1;
-        displayValue = screenText.textContent; 
-    });
-}
-
-function selectPercentBtn() {
-    percentBtn.addEventListener("click", () => {
-        screenText.textContent = parseFloat(screenText.textContent) / 100;
-        displayValue = screenText.textContent;
-    });
 }
 
 function buttonColorFlash(button, bgColor1, bgColor2, textColor1 = null, textColor2 = null) {
@@ -147,16 +135,3 @@ function buttonColorFlash(button, bgColor1, bgColor2, textColor1 = null, textCol
         }
     }, 200);
 }
-
-function init() {
-    selectNumber();
-    selectOperator();
-    selectEquals();
-    clear();
-    selectFuncBtn();
-    selectPlusMinusBtn();
-    selectPercentBtn();
-    selectDecimal();
-}
-
-init();
